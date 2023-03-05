d51c6dc5d397f16a49350fb02c225addeab894e8
luuthieukhue <luukhue0506@gmail.com>
2023-03-05 22:18:08 +0700

    Commit 05/03/2023

---
+++ src/route/web.js
@@ -0,0 +1,15 @@
+//Chạy file này đầu tiên khi chạy app
+import express from "express";
+import homeController from "../controllers/homeController";
+// import homeController, { getHomePage } from "../controllers/homeController";
+
+let router = express.Router();
+
+let initWebRoutes = (app)=>{
+
+    router.get('/',homeController.getHomePage);
+
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;  
